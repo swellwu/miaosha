6d1e8c16a97cf2572b24a6f200285356b7838352
xinjian.wu <xinjian.wu@chinaredstar.com>
2018-05-07 19:51:07 +0800

    启动成功，流程ok

--- src/main/java/wang/moshu/task/GoodsRedisStoreInitTask.java
+++ src/main/java/wang/moshu/task/GoodsRedisStoreInitTask.java
@@ -12,31 +12,28 @@ import wang.moshu.model.Goods;
 
 /**
  * 商品redis库存初始化任务
- * 
+ *
  * @category @author xiangyong.ding@weimob.com
  * @since 2017年4月9日 上午11:47:50
  */
 @Component
-public class GoodsRedisStoreInitTask
-{
-	@Autowired
-	private GoodsMapper goodsMapper;
+public class GoodsRedisStoreInitTask {
+    @Autowired
+    private GoodsMapper goodsMapper;
 
-	@Autowired
-	private GoodsRedisStoreCache goodsRedisStore;
+    @Autowired
+    private GoodsRedisStoreCache goodsRedisStore;
 
-	/**
-	 * 每隔1分钟触发一次
-	 */
-	@Scheduled(cron = "0 0/1 * * * ? ")
-	public void doInit()
-	{
-		List<Goods> goods = goodsMapper.selectAll();
-		for (Goods item : goods)
-		{
-			goodsRedisStore.doInit(item);
+    /**
+     * 每隔1分钟触发一次
+     */
+    @Scheduled(cron = "0 0/1 * * * ? ")
+    public void doInit() {
+        List<Goods> goods = goodsMapper.selectAll();
+        for (Goods item : goods) {
+            goodsRedisStore.doInit(item);
 
-		}
-	}
+        }
+    }
 
 }
